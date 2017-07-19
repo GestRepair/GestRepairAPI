f5a221e3116ae99bd01ee8a543056ed12b89957c
barcelosrui <barcelos.rui@gmail.com>
2017-07-20 00:35:58 +0100

    Activate account by email

--- dbsql.js
+++ dbsql.js
@@ -1,4 +1,5 @@
-var mysql = require('mysql');
+var mysql = require('mysql'),
+  emailjs = require('emailjs');
 
 var pool  = mysql.createPool({
   host     : 'localhost',
@@ -7,6 +8,35 @@ var pool  = mysql.createPool({
   database : 'gestrepair'
 });
 
+var email = {
+  'smtp': 'in-v3.mailjet.com',
+  'user': '',
+  'password': ''
+};
+
+var sendMessage = function(message, destination, subject, from, callback) {
+  var connection = emailjs.server.connect({
+    user: email.user,
+    password: email.password,
+    host: email.smtp,
+    port: 25
+  });
+
+  connection.send({
+    text: message,
+    'reply-to': from || email.user,
+    from: 'barcelos.rui@gmail.com',
+    to: destination,
+    subject: subject || 'GestRepair'
+  }, function(err, message) {
+    if (callback) callback(err, message);
+  });
+};
+
 module.exports = {
-  'mysql': pool
+  'url' :'http://localhost:8080',
+  'mysql': pool,
+  'email': sendMessage,
+  /*'photoPath': __dirname + "/photo",
+  'defaultPhoto': 'default_photo.png'*/
 };

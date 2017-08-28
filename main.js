4a46c5516d011005e2a6518b4086edc9411404a1
barcelosrui <barcelos.rui@gmail.com>
2017-08-28 01:24:21 +0100

    Update some queries and Create security on connection to database

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 var server = require('./server');
-
-server.start();
+var dbsql = require('./dbsql.js');
+dbsql.mysql.getConnection(function(err){
+  if(!err) {
+    console.log("Database is connected ... nn");
+    server.start();
+  } else {
+    console.log("Error connecting database ... nn");
+  }
+});
